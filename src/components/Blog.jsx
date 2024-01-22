44c0c3f7842765ca999dcd9bdc9b8c3bfef66a8e
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-01-22 02:41:13 -0800

    5.7 Blog list frontend, step7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,18 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState, } from 'react';
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const [showDetails, setShowDetails] = useState(false)
+  return <div style={blogStyle}>
+    {blog.title} <button onClick={() => setShowDetails(!showDetails)}>{showDetails ? "Hide details" : "Show details"}</button>
+    {showDetails && <div>Author: {blog.author}, url: {blog.url}, likes: {blog.likes}</div>}
+  </div>
+}
 
 export default Blog
