f3cf1e59431ab0cb156fb200edc7dbf7d3b3e2f2
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-01-22 05:11:48 -0800

    5.15: Blog list tests, step3

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -2,6 +2,43 @@ import '@testing-library/jest-dom';
 import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import Blog from './Blog';
+import blogService from '../services/blogs'; // Import the service
+
+jest.mock('../services/blogs'); // Mock the blogService module
+
+test('like button event handler is called twice when clicked twice', () => {
+  const blog = {
+    title: 'Test Blog Title',
+    author: 'Test Author',
+    url: 'http://testblog.com',
+    likes: 5,
+    user: { name: 'Test User' },
+    _id: 'testId' // Mock _id for the blog
+  };
+
+  const mockSendNotification = jest.fn();
+  const mockGetBlogsFromApi = jest.fn();
+
+  // Reset mock before each test
+  blogService.updateBlog.mockClear();
+  mockSendNotification.mockClear();
+
+  const component = render(
+    <Blog blog={blog} sendNotification={mockSendNotification} getBlogsFromApi={mockGetBlogsFromApi} user={{ name: 'Test User' }} />
+  );
+
+  // Simulate clicking the 'Show details' button
+  const detailsButton = component.getByText('Show details');
+  fireEvent.click(detailsButton);
+
+  // Find the like button and click it twice
+  const likeButton = component.getByText('Like');
+  fireEvent.click(likeButton);
+  fireEvent.click(likeButton);
+
+  // Assert that the blogService.updateBlog function was called exactly twice
+  expect(blogService.updateBlog).toHaveBeenCalledTimes(2);
+});
 
 test('renders content', () => {
   const blog = {
